c4b0c6a2ff69263dcad7157fac8dcfd9382fd433
parvsehgal <parvsehgal2004@gmail.com>
2023-08-26 21:57:01 +0530

    login and Home page

--- client/src/components/Routercomp.js
+++ client/src/components/Routercomp.js
@@ -1,11 +1,14 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Landing from "../pages/Landing";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
 function Routercomp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<>parv is on Login page</>} />
+        <Route path="/auth" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
